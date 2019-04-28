c72096ffe0b2f2412d5fb2327560deed3fa7d050
Daniel Felizardo <daniel.david772@gmail.com>
2019-04-28 00:37:22 -0300

    retirando propriedade useMongoClient

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const config = require("../config");
 
 mongoose.connect(config.url, {
-  useMongoClient: true,
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
